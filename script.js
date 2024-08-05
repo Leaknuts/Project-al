5184bf74bef7418c537d8432d2206e886d3b15a0
Leaknuts <udeanhd@gmail.com>
2024-08-05 20:44:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const menuList = document.getElementById("menu-list");
 const Hidden = document.getElementById("hidden");
 
+let menuOpen = false;
+menuList.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuList.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuList.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
 menuList.addEventListener("click", () => {
     Hidden.classList.toggle("hidden")
 })
